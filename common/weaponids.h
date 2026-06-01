f5aa24cb42229e9d23523a1f754e9cb1ab1d4714
Giannnxx <gianmccane9@gmail.com>
2026-06-01 05:18:40 -0400

    Create weaponids.h

---
+++ common/weaponids.h
@@ -0,0 +1,32 @@
+#pragma once
+
+// CS 1.6 weapon IDs (basic set)
+
+#define WEAPON_NONE        0
+#define WEAPON_P228        1
+#define WEAPON_GLOCK       2
+#define WEAPON_SCOUT       3
+#define WEAPON_HEGRENADE   4
+#define WEAPON_XM1014      5
+#define WEAPON_C4          6
+#define WEAPON_MAC10       7
+#define WEAPON_AUG         8
+#define WEAPON_SMOKEGRENADE 9
+#define WEAPON_ELITE       10
+#define WEAPON_FIVESEVEN   11
+#define WEAPON_UMP45       12
+#define WEAPON_SG550       13
+#define WEAPON_GALIL      14
+#define WEAPON_FAMAS      15
+#define WEAPON_USP        16
+#define WEAPON_AWP        18
+#define WEAPON_MP5N       19
+#define WEAPON_M249       20
+#define WEAPON_M3         21
+#define WEAPON_M4A1       22
+#define WEAPON_TMP        23
+#define WEAPON_G3SG1      24
+#define WEAPON_DEAGLE     26
+#define WEAPON_SG552      27
+#define WEAPON_AK47       28
+#define WEAPON_P90        29
